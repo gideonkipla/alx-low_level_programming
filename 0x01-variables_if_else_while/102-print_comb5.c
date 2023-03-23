03304f15d0b4fd0334ac195dda32da21508b2560
gideonkipla <monseignuergk@gmail.com>
2023-03-23 10:06:03 -0700

    Sofware is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,19 +1,43 @@
 #include <stdio.h>
 
-int main(void) {
-    int i, j;
-    for (i = 0; i < 100; i++) {
-        for (j = i; j < 100; j++) {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-            putchar(' ');
-            putchar(j / 10 + '0');
-            putchar(j % 10 + '0');
-            if (i != 99 || j != 99) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    return 0;
+/**
+ *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int tens;
+	int ones;
+	int t;
+	int o;
+
+	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
+	{
+		for (ones = '0'; ones <= '9'; ones++)
+		{
+			for (t = tens; t <= '9'; t++) /*print second of pair*/
+			{
+				for (o = ones + 1; o <= '9'; o++)
+				{
+					putchar(tens);
+					putchar(ones);
+					putchar(' ');
+					putchar(t);
+					putchar(o);
+
+					if (!((tens == '9' && ones == '8') &&
+					      (t == '9' && o == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				o = '0';
+			}
+		}
+	}
+	putchar(\n');
+
+	return (0);
 }
