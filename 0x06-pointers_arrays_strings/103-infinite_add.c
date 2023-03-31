d876ebb6926de5b3b83b57091f0e23a39f35141d
gideonkipla <monseignuergk@gmail.com>
2023-03-31 16:12:29 -0700

    it is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -7,22 +7,22 @@
  * @size_r: size of array r
  *
  * Return: the summed string in r. If r is too small for the result,
- * return NULL;
+ * return 0;
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int carry = 0, index = 0;
+	int carry = 0, index = 0, index2;
 	char *s1 = n1, *s2 = n2;
 
-	while (*s1 != '\0')
+	while (*s1 != 0)
 		s1++;
-	while (*s2 != '\0')
+	while (*s2 != 0)
 		s2++;
 	size_r--;
-	r[size_r] = '\0';
+	r[size_r] = 0;
 	s1--;
 	s2--;
-	while (s2 >= n2 && s1 >= n1)
+	while (s2 != n2 - 1 && s1 != n1 - 1)
 	{
 		r[index] = *s2 - '0' + *s1 + carry;
 		carry = 0;
@@ -34,10 +34,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		index++;
 		s2--;
 		s1--;
-		if (size_r == index && (s1 >= n1 || s2 >= n2 || carry == 1))
-			return NULL;
+		if (size_r == index && (s1 != n1 - 1 || s2 != n2 - 1 || carry == 1))
+			return (0);
 	}
-	while (s1 >= n1)
+	while (s1 != n1 - 1)
 	{
 		r[index] = *s1 + carry;
 		carry = 0;
@@ -48,10 +48,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		}
 		s1--;
 		index++;
-		if (size_r == index && (s1 >= n1 || carry == 1))
-			return NULL;
+		if (size_r == index && (s1 != n1 - 1 ||  carry == 1))
+			return (0);
 	}
-	while (s2 >= n2)
+	while (s2 != n2 - 1)
 	{
 		r[index] = *s2 + carry;
 		carry = 0;
@@ -62,19 +62,19 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		}
 		s2--;
 		index++;
-		if (size_r == index && (s2 >= n2 || carry == 1))
-			return NULL;
+		if (size_r == index && (s2 != n2 - 1 || carry == 1))
+			return (0);
 	}
 	if (carry == 1)
 	{
 		r[index] = '1';
-		r[index + 1] = '\0';
+		r[index + 1] = 0;
 	}
 	else
 	{
-		r[index--] = '\0';
+		r[index--] = 0;
 	}
-	int index2 = 0;
+	index2 = 0;
 	while (index2 <= index)
 	{
 		carry = r[index];
@@ -83,5 +83,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		index--;
 		index2++;
 	}
-	return r;
+	return (r);
 }
