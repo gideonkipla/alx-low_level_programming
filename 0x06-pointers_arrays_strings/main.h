d7c71deca034bccb0514a849510135a86b083e8b
gideonkipla <monseignuergk@gmail.com>
2023-03-31 12:53:47 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,108 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
+int _putchar(char c);
+
+/**
+  * _strcat - Concatenates two strings
+  * @dest: The destination string
+  * @src: The source string
+  *
+  * Return: A pointer to the resulting string dest
+  */
+
+char *_strcat(char *dest, char *src);
+
+/**
+  * _strncat - Concatenates two strings
+  * @dest: The destination value
+  * @src: The source value
+  * @n: The limit of the concatenation
+  *
+  * Return: A pointer to the resulting string dest
+  */
+
+char *_strncat(char *dest, char *src, int n);
+
+/**
+  * _strncpy - Copy a string
+  * @dest: The destination value
+  * @src: The source value
+  * @n: The copy limit
+  *
+  * Return: char value
+  */
+
+char *_strncpy(char *dest, char *src, int n);
+
+/**
+  * _strcmp - Compares two strings
+  * @s1: The first string
+  * @s2: The second string
+  *
+  * Return: int value
+  */
+
+int _strcmp(char *s1, char *s2);
+
+/**
+  * reverse_array - Reverses the content of an array of integers
+  * @a: An array of integers
+  * @n: Number of elements to swap
+  *
+  * Return: empty
+  */
+
+void reverse_array(int *a, int n);
+
+/**
+  * string_toupper - Changes all lowercase letters to uppercase
+  * @p: The string will be modified
+  *
+  * Return: char var
+  */
+
+char *string_toupper(char *p);
+
+/**
+  * cap_string - ...
+  * @s: ...
+  *
+  * Return: char value
+  */
+
+char *cap_string(char *s);
+
+/**
+  * leet - Encodes a string into 1337
+  * @s: The string to encode
+  *
+  * Return: The encoded string
+  */
+
+char *leet(char *s);
+
+/**
+  * rot13 - ...
+  * @s: ...
+  *
+  * Return: ...
+  */
+
+char *rot13(char *s);
+
+/**
+  * print_number - Prints any integer with putchar
+  * @n: Number to prints
+  *
+  * Return: Nothing
+  */
+
+void print_number(int n);
+
+#endif
