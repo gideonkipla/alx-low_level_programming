6a9faf5e5be67ae72fd62cd0176a79a98f5ba6d9
gideonkipla <monseignuergk@gmail.com>
2023-04-06 04:34:23 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -28,28 +28,4 @@ unsigned int _strspn(char *s, char *accept)
 		i++;
 	}
 	return (matches); /* return num if all match till end */
-
 }
-
-/* pointer arithmetic version
-unsigned int _strspn(char *s, char *accept)
-{
-	int i = 0, j;
-	int matches = 0;
-	while (*(s + i))
-	{
-		for (j = 0; *(accept + j); j++)
-		{
-			if (*(s + i) == *(accept + j))
-			{
-				matches++;
-				break;
-			}
-			if (*(accept + j + 1) == '\0' && *(s + i) != *(accept + j))
-				return (matches);
-		}
-		i++;
-	}
-	return (matches);
-}
-*/
